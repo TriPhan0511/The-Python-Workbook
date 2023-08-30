259a6e3f83d28c87bcefe727ee6477531ff95d57
Tri Phan <phanphutri@gmail.com>
2023-08-30 17:33:43 +0700

    Finished exercise 1.16

---
+++ 1-16-exercises/exercise-01-mailing-address.py
@@ -0,0 +1,5 @@
+name = 'Tri Phan'
+street = '123 Nguyen Cong Tru street'
+district = 'Son Tra district'
+print(f'{name}')
+print(f'{street}, {district}')
---
+++ 1-16-exercises/sample_string_format.py
@@ -0,0 +1,11 @@
+x = 1.14
+print('%6.2f' % x)
+print(f'{x:6.2f}')
+
+# x = 1.2345
+# print(f'{x}')
+# print(f'{x:6f}')
+# print(f'{x:8.6f}')
+# print(f'{x:10.6f}')
+# print(f'{x:>10}')
+
