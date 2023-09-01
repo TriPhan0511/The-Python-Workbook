86d848d7af42a67cc835a48b82fd0f75fc4fd9be
Tri Phan <phanphutri@gmail.com>
2023-09-01 16:50:51 +0700

    Updated

---
+++ lesson08-while-loops-and-for-loops/loops.py
@@ -0,0 +1,23 @@
+# ======= WHILE LOOPS =======
+# value = 1
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+# value = 0
+# while value < 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+
+# value = 0
+# while value < 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print(f'Value is now equal to {value}')
