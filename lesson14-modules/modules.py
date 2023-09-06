8fd0fd1b2193e02d0916338bb51e2f5cceb58c94
Tri Phan <phanphutri@gmail.com>
2023-09-06 11:02:30 +0700

    Updated

---
+++ lesson14-modules/modules.py
@@ -0,0 +1,29 @@
+# import math
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+import kansas
+from rps_v7 import rock_paper_scissors
+
+
+# print(math.pi)
+# print(pi)
+
+# print(random.choice('123'))
+# print(type(random.choice('123')))
+
+# print(rdm.choice('123'))
+
+# print(dir(rdm))
+
+# for item in dir(rdm):
+#     print(item)
+
+# print(kansas.capital)
+# kansas.random_funcfact()
+
+# print(__name__)  # __main__
+# print(kansas.__name__)  # kansas
+
+rock_paper_scissors()
