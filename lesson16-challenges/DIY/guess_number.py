0052c4b30ac4a53fbabdce33c1d85dd753b0d3e2
Tri Phan <phanphutri@gmail.com>
2023-09-09 18:56:07 +0700

    Updated

---
+++ lesson16-challenges/DIY/guess_number.py
@@ -0,0 +1,87 @@
+import random
+import sys
+
+
+def guess_number(name='Player'):
+    # Declare some variables
+    game_count = 0
+    player_wins = 0
+    computer_wins = 0
+    player_winning_percentage = 0
+    formatted_percentage = ''
+
+    def play_game():
+
+        # Get a choice from player
+        player_choice = input(
+            f'\n{name}, guess which number I\'m thinking of... 1, 2, or 3.\n\n')
+        if player_choice not in ['1', '2', '3']:
+            print(f'\n{name}, please enter 1, 2, or 3.')
+            return play_game()
+        player = int(player_choice)
+
+        # Generate a random choice
+        computer = int(random.choice('123'))
+
+        # Display both of choices
+        print(f'\n{name}, you chose {player}.')
+        print(f'I was thinking about the number {computer}.')
+
+        # Make the variables be able to be modified
+        nonlocal game_count
+        nonlocal player_wins
+        nonlocal computer_wins
+        nonlocal player_winning_percentage
+        nonlocal formatted_percentage
+
+        # Game logic
+        if player == computer:
+            player_wins += 1
+            print(f'\n🎉 {name}, you win!')
+
+        else:
+            computer_wins += 1
+            print(f'\nSorry, {name}. Better luck next time. 😢')
+
+        # Statistics
+        game_count += 1
+        formatted_percentage = f'{(player_wins / game_count):.2%}'
+        if formatted_percentage[-3] == '0':
+            formatted_percentage = f'{formatted_percentage[:-4]}%'
+        print(f'\nGame count: {game_count}')
+        print(f"\n{name}'s wins: {player_wins}")
+        print(f'\nYour winning percentage: {formatted_percentage}')
+
+        # Play again
+        while True:
+            print(f'\nPlay again, {name}?')
+            play_again = input('\nY for Yes or\nQ to Quit\n')
+            play_again = play_again.lower()
+            if play_again not in ['y', 'q']:
+                print(f'\n{name}, please enter Y or Q.')
+                continue
+            elif play_again == 'y':
+                return play_game()
+            else:
+                print(f'\n🎉🎉🎉🎉\nThank you for playing!\n\nBye {name}! 👋')
+                break
+
+    return play_game
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description='Provides a personalized game experience.')
+    parser.add_argument('-n', '--name', metavar='name',
+                        help='The name of the person playing the game.')
+    args = parser.parse_args()
+
+    if not args.name:
+        game = guess_number()
+    else:
+        game = guess_number(args.name)
+
+    # Play game
+    game()
--- lesson16-challenges/guess_number.py
+++ lesson16-challenges/guess_number.py
@@ -1,72 +1,62 @@
-import random
 import sys
+import random
 
 
-def guess_number(name='Player'):
-    # Declare some variables
+def guess_number(name='PlayerOne'):
     game_count = 0
     player_wins = 0
-    computer_wins = 0
-    player_winning_percentage = 0
-    formatted_percentage = ''
 
-    def play_game():
-
-        # Get a choice from player
+    def play_guess_number():
         player_choice = input(
-            f'\n{name}, guess which number I\'m thinking of... 1, 2, or 3.\n\n')
+            f"\n{name}, guess which number I'm thinking of... 1, 2, or 3.\n\n")
         if player_choice not in ['1', '2', '3']:
             print(f'\n{name}, please enter 1, 2, or 3.')
-            return play_game()
+            return play_guess_number()
         player = int(player_choice)
 
-        # Generate a random choice
         computer = int(random.choice('123'))
 
-        # Display both of choices
         print(f'\n{name}, you chose {player}.')
-        print(f'I was thinking about the number {computer}.')
+        print(f'I was thinking about the number {computer}.\n')
 
-        # Make the variables be able to be modified
+        def decide_winner(player, computer):
+            nonlocal player_wins
+            if player == computer:
+                player_wins += 1
+                return f'🎉 {name}, you win!'
+            else:
+                return f'Sorry, {name}. Better luck next time.'
+
+        print(decide_winner(player, computer))
+
         nonlocal game_count
-        nonlocal player_wins
-        nonlocal computer_wins
-        nonlocal player_winning_percentage
-        nonlocal formatted_percentage
-
-        # Game logic
-        if player == computer:
-            player_wins += 1
-            print(f'\n🎉 {name}, you win!')
-
-        else:
-            computer_wins += 1
-            print(f'\nSorry, {name}. Better luck next time. 😢')
-
-        # Statistics
         game_count += 1
-        formatted_percentage = f'{(player_wins / game_count):.2%}'
-        if formatted_percentage[-3] == '0':
-            formatted_percentage = f'{formatted_percentage[:-4]}%'
+
         print(f'\nGame count: {game_count}')
-        print(f"\n{name}'s wins: {player_wins}")
-        print(f'\nYour winning percentage: {formatted_percentage}')
+        print(f"\n{name}'s win: {player_wins}")
+        print(f'\nYour winning percentage: {player_wins / game_count: .2%}')
 
         # Play again
+        print(f'Play again, {name}?')
         while True:
-            print(f'\nPlay again, {name}?')
-            play_again = input('\nY for Yes or\nQ to Quit\n')
+            play_again = input('\nY for Yes or \nQ to Quit\n')
             play_again = play_again.lower()
             if play_again not in ['y', 'q']:
-                print(f'\n{name}, please enter Y or Q.')
                 continue
-            elif play_again == 'y':
-                return play_game()
             else:
-                print(f'\n🎉🎉🎉🎉\nThank you for playing!\n\nBye {name}! 👋')
                 break
 
-    return play_game
+        if play_again == 'y':
+            return play_guess_number()
+        else:
+            print('\n🎉🎉🎉🎉')
+            print('Thank you for playing!\n')
+            if __name__ == '__main__':
+                sys.exit(f'By {name}! 👋')
+            else:
+                return
+
+    return play_guess_number
 
 
 if __name__ == '__main__':
@@ -76,12 +66,12 @@ if __name__ == '__main__':
         description='Provides a personalized game experience.')
     parser.add_argument('-n', '--name', metavar='name',
                         help='The name of the person playing the game.')
+
     args = parser.parse_args()
-
     if not args.name:
-        game = guess_number()
+        guess_my_number = guess_number()
     else:
-        game = guess_number(args.name)
+        guess_my_number = guess_number(args.name)
 
-    # Play game
-    game()
+    # Play the game
+    guess_my_number()
